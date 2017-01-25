e70a44b125b92cc3cc0d77fe4d37e7cc1c4873a3
Torben <mail@sauce-code.com>
2017-01-25 19:49:07 +0100

    set line width to 100

--- src/model/interfaces/BaghChalI.java
+++ src/model/interfaces/BaghChalI.java
@@ -2,8 +2,8 @@ package model.interfaces;
 
 /**
  * Interface for a Bagh Chal game model. See
- * <a href="https://en.wikipedia.org/wiki/Bagh-Chal">https://en.wikipedia.org/
- * wiki/Bagh-Chal</a> for more information.
+ * <a href="https://en.wikipedia.org/wiki/Bagh-Chal">https://en.wikipedia.org/ wiki/Bagh-Chal</a>
+ * for more information.
  * 
  * @author Torben Kr&uuml;ger
  *
@@ -25,24 +25,22 @@ public interface BaghChalI {
 	public static final int HEIGHT = 5;
 
 	/**
-	 * Sets a goat on a desired tile. If the desired tile is not empty or there
-	 * are no goats left to set, nothing happens.
+	 * Sets a goat on a desired tile. If the desired tile is not empty or there are no goats left to
+	 * set, nothing happens.
 	 * 
 	 * @param row
 	 *            row of the desired tile
 	 * @param column
 	 *            column of the desired tile
-	 * @return {@code true}, if the desired tile is free and there is at least 1
-	 *         goat left to set
+	 * @return {@code true}, if the desired tile is free and there is at least 1 goat left to set
 	 * 
 	 * @since v0.1
 	 */
 	public boolean set(int row, int column);
 
 	/**
-	 * Selects a desired tile in order to perform a move. Therefore, the desired
-	 * tile must contain a figure of the current player. If the selection hasn't
-	 * been successful, nothing happens.
+	 * Selects a desired tile in order to perform a move. Therefore, the desired tile must contain a
+	 * figure of the current player. If the selection hasn't been successful, nothing happens.
 	 * 
 	 * @param row
 	 *            row of the desired tile
@@ -55,10 +53,9 @@ public interface BaghChalI {
 	public boolean select(int row, int column);
 
 	/**
-	 * Moves the previously selected figure by {@link #select(int, int)} to a
-	 * desired location. If no figure has been selected previously or the
-	 * desired location is invalid, nothing happens. If the move is successful,
-	 * the selected figure will be unselected.
+	 * Moves the previously selected figure by {@link #select(int, int)} to a desired location. If
+	 * no figure has been selected previously or the desired location is invalid, nothing happens.
+	 * If the move is successful, the selected figure will be unselected.
 	 * 
 	 * @param targetRow
 	 *            row of the desired target tile
@@ -114,8 +111,8 @@ public interface BaghChalI {
 	/**
 	 * Tells wether the game is over or not.
 	 * 
-	 * @return {@code true}, if the tigers capture five goats, or the goats have
-	 *         blocked the tigers from being able to move
+	 * @return {@code true}, if the tigers capture five goats, or the goats have blocked the tigers
+	 *         from being able to move
 	 * 
 	 * @since v0.1
 	 */
