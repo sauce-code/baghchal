85ae2d433329ad9353704cf481334d12ae6766b7
Torben <mail@sauce-code.com>
2017-07-12 16:56:10 +0200

    move selection into BaghChalI

--- src/model/interfaces/BaghChalI.java
+++ src/model/interfaces/BaghChalI.java
@@ -89,7 +89,7 @@ public interface BaghChalI {
 	 * Returns the current selected tile.
 	 * 
 	 * @return
-	 * 		<ul>
+	 *         <ul>
 	 *         <li>current selected tile</li>
 	 *         <li>{@code null}, if there is currently no selected tile</li>
 	 *         </ul>
@@ -199,6 +199,69 @@ public interface BaghChalI {
 		 * The game is over. Tiger player won.
 		 */
 		TIGER_WON;
+
+	}
+
+	/**
+	 * Represents a selection of a certain tile.
+	 * 
+	 * @author Torben Kr&uuml;ger
+	 *
+	 */
+	public class Selection {
+
+		/**
+		 * Row of the selection.
+		 */
+		public final int row;
+
+		/**
+		 * Column of the selection.
+		 */
+		public final int column;
+
+		/**
+		 * Creates a new selection.
+		 * 
+		 * @param row
+		 *            row
+		 * @param column
+		 *            column
+		 */
+		public Selection(int row, int column) {
+			this.row = row;
+			this.column = column;
+		}
+
+		@Override
+		public String toString() {
+			return "[row=" + row + ", column=" + column + "]";
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + column;
+			result = prime * result + row;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Selection other = (Selection) obj;
+			if (column != other.column)
+				return false;
+			if (row != other.row)
+				return false;
+			return true;
+		}
 
 	}
 
