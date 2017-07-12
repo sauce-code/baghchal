e8bb9ac1e8b9c76e1593709c0086f3db1bc5321c
Torben <mail@sauce-code.com>
2017-07-12 16:16:25 +0200

    add scoreboard

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -25,14 +25,22 @@ import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import model.BaghChal;
 import model.interfaces.BaghChalI;
+import model.interfaces.BaghChalI.State;
 import model.interfaces.Player;
 import model.interfaces.Selection;
 
@@ -80,6 +88,11 @@ public class MainWindow extends Application {
 	private HashMap<Player, Color> colorsStroke;
 
 	/**
+	 * Stores the corresponding Strings for each {@link State}.
+	 */
+	private HashMap<State, String> stateStrings;
+
+	/**
 	 * The color being used for strokes of selected pieces.
 	 */
 	private Color colorSelected = Color.CYAN;
@@ -95,9 +108,24 @@ public class MainWindow extends Application {
 	private MenuItem redo;
 
 	/**
-	 * Initializes the color maps.
+	 * Displays the text for current number of {@link BaghChal#getGoatsLeftToSet() on {@link #game}.
 	 */
-	private void initColorMaps() {
+	private Text goatsLeftToSet;
+
+	/**
+	 * Displays the text for current number of {@link BaghChal#getGoatsEaten()} on {@link #game}.
+	 */
+	private Text goatsEaten;
+
+	/**
+	 * Displays the text for current {@link BaghChal#getState()() on {@link #game}.
+	 */
+	private Text state;
+
+	/**
+	 * Initializes the maps.
+	 */
+	private void initMaps() {
 		colorsFill = new HashMap<Player, Color>();
 		colorsFill.put(Player.NONE, Color.TRANSPARENT);
 		colorsFill.put(Player.GOAT, Color.WHITE);
@@ -107,6 +135,15 @@ public class MainWindow extends Application {
 		colorsStroke.put(Player.NONE, Color.TRANSPARENT);
 		colorsStroke.put(Player.GOAT, Color.BLACK);
 		colorsStroke.put(Player.TIGER, Color.BLACK);
+
+		stateStrings = new HashMap<State, String>();
+		stateStrings.put(State.GOAT_MOVE, "goat's turn");
+		stateStrings.put(State.GOAT_SELECT, "goat's turn");
+		stateStrings.put(State.GOAT_SET, "goat's turn");
+		stateStrings.put(State.GOAT_WON, "goats won!");
+		stateStrings.put(State.TIGER_MOVE, "tiger's turn");
+		stateStrings.put(State.TIGER_SELECT, "tiger's turn");
+		stateStrings.put(State.TIGER_WON, "tigers won!");
 	}
 
 	/**
@@ -322,6 +359,47 @@ public class MainWindow extends Application {
 		return ret;
 	}
 
+	private VBox initScoreboard() {
+		// scoreBoard.setPrefWidth(200);
+
+		goatsLeftToSet = new Text("Goats left to set: " + game.getGoatsLeftToSet());
+		goatsLeftToSet.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 20.0));
+		goatsLeftToSet.setTextAlignment(TextAlignment.CENTER);
+		BorderPane box1 = new BorderPane(goatsLeftToSet);
+		// box1.setPrefSize(400.0, 50.0);
+		// box1.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+//		box1.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
+//				CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
+		goatsEaten = new Text("Goats eaten: " + game.getGoatsEaten());
+		goatsEaten.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 20.0));
+		BorderPane box2 = new BorderPane(goatsEaten);
+		// box2.setPrefSize(400.0, 50.0);
+		// box2.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+//		box2.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
+//				CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		
+		HBox box0 = new HBox(box1, box2);
+		box0.setPadding(new Insets(6.0, 0.0, 0.0, 0.0));
+		HBox.setHgrow(box1, Priority.ALWAYS);
+		HBox.setHgrow(box2, Priority.ALWAYS);
+//		box0.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
+//				CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
+		state = new Text(stateStrings.get(game.getState()));
+		state.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 25.0));
+		BorderPane box3 = new BorderPane(state);
+		box3.setPadding(new Insets(2.0, 0.0, 0.0, 0.0));
+		// box3.setPrefSize(400.0, 50.0);
+		// box3.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+//		box3.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,
+//				CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
+		VBox scoreboard = new VBox(box0, box3);
+//		scoreboard.setBorder(new Border(new BorderStroke(null, null, Color.BLACK, null, null, null, BorderStrokeStyle.SOLID, null, CornerRadii.EMPTY, new BorderWidths(4.0), null)));
+		return new VBox(scoreboard);
+	}
+
 	/**
 	 * Refreshes the whole GUI.
 	 */
@@ -343,6 +421,11 @@ public class MainWindow extends Application {
 			tiles[selected.row][selected.column].setStroke(colorSelected);
 		}
 
+		// set texts
+		goatsLeftToSet.setText("gaots left to set: " + game.getGoatsLeftToSet());
+		goatsEaten.setText("Goats eaten: " + game.getGoatsEaten());
+		state.setText(stateStrings.get(game.getState()));
+
 		// set status bar text
 		statusBar.setText("goats eaten: " + game.getGoatsEaten() + "    goats left to set: "
 				+ game.getGoatsLeftToSet() + "    State: " + game.getState().toString());
@@ -354,13 +437,14 @@ public class MainWindow extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		initColorMaps();
+		initMaps();
 
-		VBox vBox = new VBox(initBoard());
+		VBox vBox = new VBox(initScoreboard(), initBoard());
 		statusBar = new StatusBar();
 
 		BorderPane border = new BorderPane(vBox);
 		border.setTop(initMenuBar());
+
 		border.setBottom(statusBar);
 
 		Scene scene = new Scene(border);
