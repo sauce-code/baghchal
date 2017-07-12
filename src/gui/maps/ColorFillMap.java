1135809a54393ab5afa87078a48d0e6dc7b80d72
Torben <mail@sauce-code.com>
2017-07-12 16:52:11 +0200

    add javadoc

--- src/gui/maps/ColorFillMap.java
+++ src/gui/maps/ColorFillMap.java
@@ -5,13 +5,22 @@ import java.util.HashMap;
 import javafx.scene.paint.Color;
 import model.interfaces.Player;
 
+/**
+ * Stores the colors used for filling pieces.
+ * 
+ * @author Torben Kr&uuml;ger
+ *
+ */
 public class ColorFillMap extends HashMap<Player, Color> {
 
 	/**
-	 * 
+	 * Serial Version UID.
 	 */
 	private static final long serialVersionUID = 8483911894826353187L;
 
+	/**
+	 * Creates a new Color Fill Map.
+	 */
 	public ColorFillMap() {
 		super();
 		put(Player.NONE, Color.TRANSPARENT);
--- src/gui/maps/StateStringMap.java
+++ src/gui/maps/StateStringMap.java
@@ -4,13 +4,22 @@ import java.util.HashMap;
 
 import model.interfaces.BaghChalI.State;
 
+/**
+ * Stores the corresponding Strings for each {@link State}.
+ * 
+ * @author Torben Kr&uuml;ger
+ *
+ */
 public class StateStringMap extends HashMap<State, String> {
 
 	/**
-	 * 
+	 * Serial Version UID.
 	 */
 	private static final long serialVersionUID = 6356717555055526293L;
 	
+	/**
+	 * Creates a new State String Map.
+	 */
 	public StateStringMap() {
 		super();
 		put(State.GOAT_MOVE, "goat's turn");
